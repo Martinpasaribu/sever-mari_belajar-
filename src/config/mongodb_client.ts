f27f71e387018bdef581351dde72490955e72491
Martinpasaribu <83689864+Martinpasaribu@users.noreply.github.com>
2024-09-19 04:59:28 +0700

    5

--- src/config/mongodb_client.ts
+++ src/config/mongodb_client.ts
@@ -1,24 +1,5 @@
 
 
-// import {MongoClient,Db} from "mongodb";
-
-
-// let mongoDb : Db
-
-
-// export async function connectToDatabase(){
-//     const  url = 'mongodb://127.0.0.1:27017'
-//     const  client = new MongoClient(url);
-//     mongoDb = client.db("Flutter_app(Mari_Belajar)")
-//     console.log("mongodb connected successfully")
-// }
-
-// export function getDatabase() : Db{
-//     return mongoDb
-// }
-
-
-
 import { MongoClient, Db } from "mongodb";
 import dotenv from 'dotenv';
 
@@ -46,4 +27,25 @@ export function getDatabase(): Db {
     }
     return mongoDb;
 }
+
+
+
+
+
+// import {MongoClient,Db} from "mongodb";
+
+
+// let mongoDb : Db
+
+
+// export async function connectToDatabase(){
+//     const  url = 'mongodb://127.0.0.1:27017'
+//     const  client = new MongoClient(url);
+//     mongoDb = client.db("Flutter_app(Mari_Belajar)")
+//     console.log("mongodb connected successfully")
+// }
+
+// export function getDatabase() : Db{
+//     return mongoDb
+// }
 
